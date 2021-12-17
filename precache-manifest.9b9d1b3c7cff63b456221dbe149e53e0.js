dfcc30a14575ea5174c844a9b88f325e0ac1f2b7
Jason Stover <jjayguy23@gmail.com>
2021-12-17 15:18:43 -0500

    Updates

---
+++ precache-manifest.9b9d1b3c7cff63b456221dbe149e53e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d0582f75c66b49c1ffc0794b617fa8",
+    "url": "/planetapi/index.html"
+  },
+  {
+    "revision": "c164bbd111f82c6c3feb",
+    "url": "/planetapi/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "e0b92fc5ca9cb48c24d3",
+    "url": "/planetapi/static/css/main.160019ec.chunk.css"
+  },
+  {
+    "revision": "c164bbd111f82c6c3feb",
+    "url": "/planetapi/static/js/2.cbccdc0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/planetapi/static/js/2.cbccdc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b92fc5ca9cb48c24d3",
+    "url": "/planetapi/static/js/main.10d90d54.chunk.js"
+  },
+  {
+    "revision": "4277ca919319604c90e5",
+    "url": "/planetapi/static/js/runtime-main.09fe40b4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/planetapi/static/media/SEGA.12a3db8b.woff"
+  }
+]);
